2110c1f1edf301c7b6fc1748f823a8631437193e
Sven Starcke <sst97@gmx.net>
2019-09-20 09:49:12 +0200

    resetted project structure for starting with pure java client

--- generate.sh
+++
@@ -1,8 +0,0 @@
-java -jar submodules/JNAerator/jnaerator/target/jnaerator-0.13-SNAPSHOT-shaded.jar  \
-  -library capnzero-monitoring \
-  -mode StandaloneJar \
-  -I submodules/capnzero-monitoring/include \
-  -I submodules/capnzero-monitoring/submodules/capnzero/capnzero/include \
-  submodules/capnzero-monitoring/lib/libcapnzeroMonitoring.so \
-  submodules/capnzero-monitoring/include/**.h
-  
